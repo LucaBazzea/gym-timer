52f68a06f83e26b220e56934ac38e0eae780ae7e
Luca Bazzea <luca.bazzea@gmail.com>
2020-03-27 16:54:58 +0200

    initial

---
+++ timer.py
@@ -0,0 +1,29 @@
+import time
+
+# Choose how many rounds
+while True:
+    try:
+        round_num = int(input("Number of rounds: "))
+        break
+    except:
+        print("Input numbers only")
+        print("---")
+
+# Choose duration
+while True:
+    try:
+        round_len = int(input("Round length: "))
+        break
+    except:
+        print("Input numbers only")
+        print("---")
+
+# Choose rest
+
+# Go again or new clock
+
+
+
+
+
+# Could be cool to make this a SBG themed clock with motivational quotes from SBG athletes
