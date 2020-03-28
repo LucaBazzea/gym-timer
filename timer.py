f9855a9765aba3835664067b5931d30f0957e620
Luca Bazzea <luca.bazzea@gmail.com>
2020-03-28 17:11:04 +0200

    Beginning of the timer functionality

--- timer.py
+++ timer.py
@@ -1,4 +1,9 @@
 import time
+import os
+
+hours = int(0) 
+minutes = int(0)
+seconds = int(0)
 
 # Choose how many rounds
 while True:
@@ -12,13 +17,27 @@ while True:
 # Choose duration
 while True:
     try:
-        round_len = int(input("Round length: "))
+        hours = int(input("Hours: "))
+        minutes = int(input("Minutes: "))
+        seconds = int(input("Seconds: "))
         break
     except:
         print("Input numbers only")
         print("---")
 
-# Choose rest
+# Choose rest, rest must be > 3 sec so that it can rest -= 3
+
+'''for i in range(seconds):
+    print("seconds: " + str(seconds - i))
+    time.sleep(1)'''
+
+while seconds > -1:
+    os.system("cls")
+    print(f"H:{hours}M:{minutes}S:{seconds}")
+    time.sleep(1)
+    seconds -= 1
+    os.system("cls")
+
 
 # Go again or new clock
 
@@ -27,3 +46,5 @@ while True:
 
 
 # Could be cool to make this a SBG themed clock with motivational quotes from SBG athletes
+
+# Create presets
